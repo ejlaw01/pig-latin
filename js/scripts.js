bb2de146457774d6be6d71edc0fd32d44fced234
Ethan Law and Kyle Lange <kyle@>
2016-08-17 16:02:14 -0700

    add functionality for multiple starting consonants and qu

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,21 @@
 //business logic
 var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
+var quArray = ["qu", "Qu", "QU"];
 var i = 0;
 
 var translator = function(input) {
-  // debugger;
+  debugger;
   var translation = [];
-  for (i = 0; i <= input.length; i += 1) {
-    if (vowels.includes(input[i])) {
-      translation.push("is a vowel");
+  for (i = 0; i < input.length; i += 1) {
+    if (vowels.includes(input[0])) {
+      translation.push(input + "ay");
+    // } else if (quArray.includes(input[0] + input[1])) {
+    //   translation.push(input.slice(2) + input[0] + input[1] + "ay");
+    } else if ((consonants.includes(input[0]) && consonants.includes(input[1])) || (quArray.includes(input[0] + input[1]))) {
+      translation.push(input.slice(2) + input[0] + input[1] + "ay");
+    } else if (consonants.includes(input[0])) {
+      translation.push(input.slice(1) + input[0] + "ay");
     } else {
       alert("not a vowel");
     }
