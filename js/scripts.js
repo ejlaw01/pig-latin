c9accd294dc3b0e97fa82b255257f0d090f234bf
Ethan Law <ejlaw01@gmail.com>
2016-08-17 20:11:42 -0700

    add capability to run sentences through translator by splitting into an array of one-word strings and sending each through the translation mechanism

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,35 @@
 //business logic
 var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
-var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
+var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z", "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"];
 var quArray = ["qu", "Qu", "QU"];
 var i = 0;
+var translation = [];
 
 var wordTranslator = function(input) {
-  var translation = [];
-  for (i = 0; i < input.length; i += 1) {
+  for (i = 0; i < input.length; i++) {
     if (vowels.includes(input[0])) {
       translation.push(input + "ay");
-    // } else if (quArray.includes(input[0] + input[1])) {
-    //   translation.push(input.slice(2) + input[0] + input[1] + "ay");
+    } else if (consonants.includes(input[0]) && consonants.includes(input[1]) && consonants.includes(input[2])) {
+      translation.push(input.slice(3) + input[0] + input[1] + input[2] + "ay");
     } else if ((consonants.includes(input[0]) && consonants.includes(input[1])) || (quArray.includes(input[0] + input[1]))) {
       translation.push(input.slice(2) + input[0] + input[1] + "ay");
     } else if (consonants.includes(input[0])) {
       translation.push(input.slice(1) + input[0] + "ay");
     } else {
-      alert("not a vowel");
+      alert ("Please enter a word.")
     }
     return translation;
   }
-};
+}
 
 //user interface logic
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
-    var splitSentence = $("input#sentence").val();
-    var pigLatinSentence = wordTranslator(splitSentence);
-
-    // var wordArray = splitSentence.forEach(function(word) {
-    //   wordTranslator(word);
-    // });
-
-    $("#output").text(pigLatinSentence);
+    var splitSentence = $("input#sentence").val().split(" ");
+    var wordArray = splitSentence.forEach(function(input) {
+      wordTranslator(input);
+    });
+    $("#output").text(translation.join(" "));
   });
 });
