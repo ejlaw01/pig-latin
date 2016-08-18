023ce9ce21175587dd11777c13a5b9890348a99d
Ethan Law <ejlaw01@gmail.com>
2016-08-17 21:54:42 -0700

    make words with only consonants and y's, like try, be correctly translated and remove an unnecessary else statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 //business logic
 var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
 var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z", "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"];
+var consonantsLessY = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z", "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "V", "W", "X", "Z"];
 var quArray = ["qu", "Qu", "QU"];
 var i = 0;
 var translation = [];
@@ -9,14 +10,12 @@ var wordTranslator = function(input) {
   for (i = 0; i < input.length; i++) {
     if (vowels.includes(input[0])) {
       translation.push(input + "ay");
-    } else if (consonants.includes(input[0]) && consonants.includes(input[1]) && consonants.includes(input[2])) {
+    } else if (consonants.includes(input[0]) && consonants.includes(input[1]) && consonantsLessY.includes(input[2])) {
       translation.push(input.slice(3) + input[0] + input[1] + input[2] + "ay");
-    } else if ((consonants.includes(input[0]) && consonants.includes(input[1])) || (quArray.includes(input[0] + input[1]))) {
+    } else if ((consonants.includes(input[0]) && consonantsLessY.includes(input[1])) || (quArray.includes(input[0] + input[1]))) {
       translation.push(input.slice(2) + input[0] + input[1] + "ay");
     } else if (consonants.includes(input[0])) {
       translation.push(input.slice(1) + input[0] + "ay");
-    } else {
-      alert ("Please enter a word.")
     }
     return translation;
   }
@@ -26,10 +25,12 @@ var wordTranslator = function(input) {
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
+    translation.length = 0
     var splitSentence = $("input#sentence").val().split(" ");
     var wordArray = splitSentence.forEach(function(input) {
       wordTranslator(input);
     });
     $("#output").text(translation.join(" "));
+    $("input").val("");
   });
 });
